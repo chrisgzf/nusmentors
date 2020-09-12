54bd27639bab4bbdcf2de94c31bd4bf7cd10576b
Christopher Goh <chrisgzf@gmail.com>
2020-09-13 06:26:17 +0800

    Use react props markup for SVG components

--- web/src/components/LogoIcon/index.js
+++ web/src/components/LogoIcon/index.js
@@ -17,13 +17,13 @@ export default function LogoIcon({ scale }) {
       />
       <path
         stroke="#fff"
-        stroke-miterlimit="10"
-        stroke-width="14"
+        strokeMiterlimit="10"
+        strokeWidth="14"
         fill="#1faaff"
         d="M263.4 22H104.6L25.3 159.5 104.6 297h158.8l79.3-137.5L263.4 22z"
       />
       <path
-        class="cls-3"
+        className="cls-3"
         d="M95.2 102h13.5l42.8 69.3h.2V102H166v97.5h-13.5l-42.7-69.3h-.3v69.3H95.2zM189 102h13.4l27 63.6 26.7-63.6h13.5v97.5h-14.3v-60.8h-.3l-19.8 46.9h-12l-19.7-46.9h-.3v60.8H189z"
       />
     </svg>
