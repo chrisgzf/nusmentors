9a1f5a9866cb95e9f03f9ce8653df8cd1dfd29c3
Christopher Goh <chrisgzf@gmail.com>
2020-09-22 07:17:16 +0800

    Add new firebase utils

---
+++ web/src/utils/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+export const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+
+export function setupFirebase() {
+  // Initialise Firebase
+  try {
+    firebase.initializeApp(firebaseConfig);
+  } catch (err) {
+    console.error("Firebase initialization error", err.stack);
+  }
+  return firebase;
+}
+
+// Selectors
+export const selectAuth = (state) => state.firebase.auth;
+export const selectJWT = (state) =>
+  state.firebase.auth.stsTokenManager
+    ? state.firebase.auth.stsTokenManager.accessToken
+    : null;
