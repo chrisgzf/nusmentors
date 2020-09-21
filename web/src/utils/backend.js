214be5fe892a73cf480b7c1a68fdca4bb2f413f1
Christopher Goh <chrisgzf@gmail.com>
2020-09-21 21:22:11 +0800

    Add useBackend hook

---
+++ web/src/utils/backend.js
@@ -0,0 +1,48 @@
+import useAuth from "auth/useAuth";
+
+const backendHost = process.env.REACT_APP_BACKEND_HOST
+  ? process.env.REACT_APP_BACKEND_HOST
+  : process.env.NODE_ENV === "development"
+  ? "http://localhost:8080"
+  : "https://someherokuurl";
+
+console.log(`Backend host: ${backendHost}`);
+
+function sendBackendRequest(endpoint, method, data, headers) {
+  const defaultHeaders = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
+  const actualHeaders = { ...defaultHeaders, ...headers };
+
+  return fetch(`${backendHost}/${endpoint}`, {
+    method, // GET POST PUT DELETE etc
+    headers: actualHeaders,
+    body: JSON.stringify(data),
+  });
+}
+
+export function useBackend() {
+  const { user } = useAuth();
+
+  async function sendRequest(endpoint, method, data, headers) {
+    let token;
+    let additionalHeaders;
+
+    // Decide if user is logged in, and if user is logged in,
+    // add JWT token to request headers
+    if (user) {
+      const idTokenResult = await user.getIdTokenResult();
+      token = idTokenResult.token;
+    }
+    if (token) {
+      additionalHeaders = { ...headers, Authorization: `Bearer ${token}` };
+    } else {
+      additionalHeaders = headers;
+    }
+    return sendBackendRequest(endpoint, method, data, additionalHeaders);
+  }
+
+  return { sendRequest };
+}
