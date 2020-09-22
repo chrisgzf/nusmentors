a852b7cf99ccf0d53f4a1deb26f343a89065d979
Aung Thuya Oo <thuyaoo200@gmail.com>
2020-09-22 19:38:51 +0800

    Add request store

--- web/src/utils/store.js
+++ web/src/utils/store.js
@@ -2,6 +2,7 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import counterReducer from "../components/Counter/counterSlice";
 import { firebaseReducer } from "react-redux-firebase";
 import uiReducer from "../slices/uiSlice";
+import requestsReducer from "../slices/requestsSlice";
 import logger from "redux-logger";
 
 export default configureStore({
@@ -9,6 +10,7 @@ export default configureStore({
     counter: counterReducer,
     firebase: firebaseReducer,
     ui: uiReducer,
+    requests: requestsReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
