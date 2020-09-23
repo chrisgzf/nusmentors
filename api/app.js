33b68d9cd6ad2c5034a61367d838ae9a1ff3ad60
Yoke Kai Xin <kxyoke@gmail.com>
2020-09-23 10:40:34 +0800

    Fix config file path.

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ const pool = require("./db");
 // Auth
 const admin = require("firebase-admin");
 
-const serviceAccount = require("config/nusmentors-firebase-adminsdk-key.json");
+const serviceAccount = require("./config/nusmentors-firebase-adminsdk-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
