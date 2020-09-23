7b0cc6d9ef04897455955adf8737bc73f5782273
Christopher Goh <chrisgzf@gmail.com>
2020-09-24 03:43:57 +0800

    Update user slice redux state

--- web/src/slices/userSlice.js
+++ web/src/slices/userSlice.js
@@ -3,11 +3,12 @@ import { sendRequest } from "utils/backend";
 
 const initialState = {
   name: "",
-  nus_email: "",
-  photo_url: "",
-  matric_date: "",
-  grad_date: "",
+  email: "",
+  nusEmail: "",
+  matricDate: "",
+  gradDate: "",
   major: "",
+  photoUrl: "",
   telegram: "",
   status: "",
   error: null,
@@ -21,6 +22,14 @@ export const fetchUserInfo = createAsyncThunk(
   },
 );
 
+export const postUserInfo = createAsyncThunk(
+  "user/postUserInfo",
+  async (data, { dispatch }) => {
+    const response = await dispatch(sendRequest("auth", "POST", data));
+    return data;
+  },
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -28,8 +37,10 @@ const userSlice = createSlice({
   extraReducers: {
     [fetchUserInfo.pending]: (state, action) => {
       state.status = "loading";
+      state.error = null;
     },
     [fetchUserInfo.fulfilled]: (state, action) => {
+      console.log(action.payload);
       state.status = "succeeded";
       state = { ...action.payload, ...state };
     },
@@ -37,17 +48,22 @@ const userSlice = createSlice({
       state.status = "failed";
       state.error = action.error.message;
     },
+    [postUserInfo.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      state = { ...action.payload, ...state };
+    },
   },
 });
 
 // Selectors
 export const selectUser = (state) => state.user;
 export const selectName = (state) => state.user.name;
-export const selectNUSEmail = (state) => state.user.nus_email;
-export const selectPhotoURL = (state) => state.user.photo_url;
-export const selectMatricDate = (state) => state.user.matric_date;
-export const selectGradDate = (state) => state.user.grad_date;
+export const selectNUSEmail = (state) => state.user.nusEmail;
+export const selectPhotoURL = (state) => state.user.photoUrl;
+export const selectMatricDate = (state) => state.user.matricDate;
+export const selectGradDate = (state) => state.user.gradDate;
 export const selectMajor = (state) => state.user.major;
 export const selectTelegram = (state) => state.user.telegram;
+export const selectUserError = (state) => state.user.error;
 
 export default userSlice.reducer;
