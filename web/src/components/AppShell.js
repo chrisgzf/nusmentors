936c3a3c4c474205b618539ee13c48bbf2442527
Christopher Goh <chrisgzf@gmail.com>
2020-09-24 06:57:01 +0800

    Kick users out of AppShell if unauthed

--- web/src/components/AppShell.js
+++ web/src/components/AppShell.js
@@ -1,8 +1,13 @@
 import { Box, Container, makeStyles, Toolbar } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
 import TopBar from "./TopBar";
 import AppDrawer from "./AppDrawer";
 import AuthRouter from "./AuthRouter";
+import { fetchUserInfo, selectUserError } from "slices/userSlice";
+import { isEmpty } from "react-redux-firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAuth, selectFBEmailVerified } from "utils/firebase";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -16,6 +21,34 @@ const useStyles = makeStyles((theme) => ({
 
 const AppShell = () => {
   const classes = useStyles();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const fbLoggedIn = !isEmpty(useSelector(selectAuth));
+  const fbEmailVerified = useSelector(selectFBEmailVerified);
+  const userError = useSelector(selectUserError);
+
+  // Fetch user data on mount
+  useEffect(() => {
+    dispatch(fetchUserInfo());
+  }, [dispatch]);
+
+  // If user has insufficient details, kick them to login page
+  useEffect(() => {
+    if (userError) {
+      history.push("/login");
+      return;
+    }
+  }, [dispatch, history, userError]);
+
+  // Kick users out of app if they are not logged in or email not verified
+  useEffect(() => {
+    if (!fbLoggedIn || !fbEmailVerified) {
+      history.push("/login");
+      return;
+    }
+  }, [fbLoggedIn, fbEmailVerified, history]);
+
   return (
     <>
       <TopBar />
