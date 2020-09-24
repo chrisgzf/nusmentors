93b9e707278aa2de8f69271fa69a0c775c8a08a4
teikjun <teikjunhci@gmail.com>
2020-09-24 18:50:30 +0800

    add more details for getnotifications

--- api/routes/impl/notifications.js
+++ api/routes/impl/notifications.js
@@ -3,9 +3,10 @@ const pool = require("../../db");
 const getnotifications = (req, res) => {
   const req_uid = req.body.req_uid;
   pool.query(
-  `SELECT U.name, N.to_id , N.notif_type, N.date_created 
-  FROM Notifies N, UsersInfo U
+  `SELECT U.name, N.to_id , N.notif_type, N.date_created, R.title, R.req_id 
+  FROM Notifies N, UsersInfo U, Requests R
   WHERE N.from_id = U.user_id
+  AND N.to_id = R.mentee_id
   AND N.to_id = $1`,
   [req_uid],
     (q_err, q_res) => {
