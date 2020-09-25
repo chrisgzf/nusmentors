efb8d9212c6cfa02ddcf558a12dad25f3ffc2669
teikjun <teikjunhci@gmail.com>
2020-09-25 22:48:48 +0800

    complete landing page

--- web/src/pages/Homepage/index.js
+++ web/src/pages/Homepage/index.js
@@ -11,35 +11,37 @@ import LogoIcon from "components/LogoIcon";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
+import ImageFirst from "./img/mentee_1.svg";
+import ImageSecond from "./img/mentor_3.svg";
+import ImageThird from "./img/mentor_2.svg";
 
 const features = [
   {
-    title: <>Easy to use </>,
-    imageUrl: "img/undraw_docusaurus_mountain.svg",
+    title: <>Look for mentors</>,
+    imgUrl: ImageFirst,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        NUSMentors helps you find mentors that are
+        proficient in your area of interest
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
-    imageUrl: "img/undraw_docusaurus_tree.svg",
+    title: <>Connect with mentees</>,
+    imgUrl: ImageSecond,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        NUSMentors connects you with mentees and
+        enables you to build your network with juniors in a meaningful way
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
-    imageUrl: "img/undraw_docusaurus_react.svg",
+    title: <>All in one platform</>,
+    imgUrl: ImageThird,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Looking to do both? You can obtain mentorship and also mentor others in NUSMentors!
       </>
     ),
   },
@@ -49,10 +51,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  title: {
-    flexGrow: 1,
-    textAlign: "left",
-  },
   buttons: {
     display: "flex",
     alignItems: "center",
@@ -60,11 +58,16 @@ const useStyles = makeStyles((theme) => ({
   },
   heroTitle: {
     color: "white",
-    textAlign: "left",
+    marginBottom: "10px",
+    fontSize: "45px",
+    textShadow: "0.5px 0.5px #A3AAAF",
+
   },
   heroSubtitle: {
     color: "white",
-    textAlign: "left",
+    marginBottom: "30px",
+    fontSize: "25px",
+    textShadow: "0.5px 0.5px #A3AAAF",
   },
   button: {
     "&:hover": {
@@ -76,9 +79,12 @@ const useStyles = makeStyles((theme) => ({
     border: "solid",
     borderColor: "aliceblue",
     textAlign: "left",
+    fontSize: "22px",
+    borderRadius: "8px",
+    marginBottom: "20px",
   },
   appHeader: {
-    minHeight: "50vh",
+    minHeight: "46vh",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -86,53 +92,72 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "calc(10px + 2vmin)",
     backgroundColor: "#1FAAFF",
   },
+  features: {
+    display: "flex",
+    alignItems: "center",
+    padding: "2rem 0",
+    width: "100%",
+  },
+  feature: {},
+  featureImage: {
+    height: "200px",
+    width: "200px",
+  },
+  description: {
+    fontSize: "18px",
+  },
+  title: {
+    fontSize: "22px",
+    marginBottom: "6px",
+  }
 }));
 
+function Feature({ imgUrl, title, description }) {
+  const classes = useStyles();
+  return (
+    <Grid item sm={4}>
+      {imgUrl && (
+        <div>
+          <img className={classes.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h1 className={classes.title}>{title}</h1>
+      <div className={classes.description}>{description}</div>
+    </Grid>
+  );
+}
 function App() {
   const classes = useStyles();
   return (
     <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h4" className={classes.title}>
-            NUSMentors
-          </Typography>
-          <Button color="inherit" href="/login">
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-
       <header className={classes.appHeader}>
-        <Container maxwidth="sm">
-        <Grid container className="container">
-          <Grid item xs={3}>
-            <LogoIcon scale="90%"/>
-          </Grid>
-          <Grid item xs={9}>
-            <h1 className={classes.heroTitle}>Welcome to NUSMentors</h1>
-            <p className={classes.heroSubtitle}>
-              If you're looking for a mentorship, you've come to the right
-              place.
-            </p>
-            <Button
-              variant="outlined"
-              size="large"
-              href="/dashboard"
-              className={classes.button}
-            >
-              Get Started
-            </Button>
-          </Grid>
-        </Grid>
-        </Container>
+        {/* <LogoIcon scale="90%"/> */}
+        <h1 className={classes.heroTitle}>Welcome to NUSMentors</h1>
+        <div className={classes.heroSubtitle}>
+          If you're looking for a mentorship, you've come to the right place!
+        </div>
+        <Button
+          variant="outlined"
+          size="large"
+          href="/dashboard"
+          className={classes.button}
+        >
+          Get Started
+        </Button>
       </header>
-
-      <h1>NUSMentors</h1>
-      <span>This is a placeholder homepage.</span>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      <main>
+        {features && features.length > 0 && (
+          <section className={classes.features}>
+            <Container>
+              <Grid container spacing={6}>
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </Grid>
+            </Container>
+          </section>
+        )}
+      </main>
     </div>
   );
 }
