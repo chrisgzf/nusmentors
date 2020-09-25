06267a0472289eb14ac5757e6e34edcb680ea61d
Christopher Goh <chrisgzf@gmail.com>
2020-09-25 14:04:36 +0800

    Revert loading spinner on app mount

--- web/src/components/App/index.js
+++ web/src/components/App/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { CircularProgress, CssBaseline } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 
 import Homepage from "pages/Homepage";
 import UserAuth from "pages/UserAuth";
 import AppShell from "../AppShell";
-import { useDispatch, useSelector } from "react-redux";
-import { selectIsFBLoaded, setupFirebase } from "utils/firebase";
+import { useDispatch } from "react-redux";
+import { setupFirebase } from "utils/firebase";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 
@@ -22,34 +22,20 @@ function App() {
     config: {},
     dispatch,
   };
-  const isFBLoaded = useSelector(selectIsFBLoaded);
 
   return (
     <ReactReduxFirebaseProvider {...rrfProps}>
       <MuiThemeProvider theme={theme}>
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <CssBaseline />
-          {isFBLoaded ? (
-            <Router>
-              <Switch>
-                <Route exact path="/" component={Homepage} />
-                <Route exact path="/register" component={UserAuth} />
-                <Route exact path="/login" component={UserAuth} />
-                <Route component={AppShell} />
-              </Switch>
-            </Router>
-          ) : (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: `${window.innerHeight}px`,
-              }}
-            >
-              <CircularProgress />
-            </div>
-          )}
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/register" component={UserAuth} />
+              <Route exact path="/login" component={UserAuth} />
+              <Route component={AppShell} />
+            </Switch>
+          </Router>
         </MuiPickersUtilsProvider>
       </MuiThemeProvider>
     </ReactReduxFirebaseProvider>
